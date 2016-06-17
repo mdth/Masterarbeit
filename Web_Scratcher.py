1712e5763407c97d6c4aa96a45cf580335f204f4
Mai Lan Dinh Thanh <dinhthan@fim.uni-passau.de>
2016-06-17 16:32:57 +0200

    Web Scratcher done

--- Web_Scratcher.py
+++ Web_Scratcher.py
@@ -10,26 +10,20 @@ def get_web_text(url):
     return div
 
 
-def write_into_file(text):
-    os.chdir("C:/Users/din_m/PycharmProjects/Masterarbeit/")
-    with open("file.txt", "w") as file:
+def write_into_file(text, filename, file_path):
+    os.chdir(file_path)
+    with open("Chapter " + str(filename) + ".txt", "w") as file:
         file.writelines(text)
 
 
-def main():
+def main(save_path):
     url = "http://gutenberg.spiegel.de/buch/der-idiot-2098/"
     page = 1
-    text = ""
     while page <= 50:
-        text += get_web_text(url + str(page))
+        text = get_web_text(url + str(page))
+        write_into_file(text, page, save_path)
         print("Read page " + str(page) + " from 50.")
         page += 1
 
-    write_into_file(text)
-
-main()
-#text = get_web_text("http://gutenberg.spiegel.de/buch/der-idiot-2098/1")
-#print(text)
-#print(text.encode('utf-8'))
-#write_into_file()
-
+path = "C:/Users/din_m/PycharmProjects/Masterarbeit/Der Idiot"
+main(path)
